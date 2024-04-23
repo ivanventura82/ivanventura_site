f2a94ae4c981b6f721e56315fca0537fe44c7181
thiago laredo <thiago@olatu.com.br>
2024-04-23 14:44:04 -0300

    implementado a animacao

--- js/modules/menu-projetos.js
+++ js/modules/menu-projetos.js
@@ -1,37 +1,75 @@
 import outsideClick from "./outsideclick.js";
+import gsap from 'gsap';
 
 export default class MenuProjetos {
   constructor(projetosButton, projetosList, events) {
     this.projetosButton = document.querySelector(projetosButton);
     this.projetosList = document.querySelector(projetosList);
-
-
     this.activeClass = "active";
 
-    // define touchstart e click como argumento padrão
-    // de events caso o usuário não define
     if (events === undefined) this.events = ["touchstart", "click"];
     else this.events = events;
 
-    this.openMenu = this.openMenu.bind(this);
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+    this.isOpen = false;  // Estado para controlar se o menu está aberto ou fechado
   }
 
-  openMenu(event) {
+  toggleMenu(event) {
     event.preventDefault();
+    if (!this.isOpen) {
+      this.openMenu();
+    } else {
+      this.closeMenu(event, true);  // Chamada direta para fechamento com um segundo argumento
+    }
+  }
+
+  openMenu() {
     this.projetosList.classList.add(this.activeClass);
     this.projetosButton.classList.add(this.activeClass);
+    const items = this.projetosList.querySelectorAll('li');
+    gsap.set(items, { opacity: 0 });
+    gsap.to(items, {
+      opacity: 1,
+      duration: 0.3,
+      stagger: {
+        amount: 0.5,
+        from: "end"
+      }
+    });
+    this.isOpen = true;
+    document.addEventListener('click', this.closeMenu);
+  }
 
+  closeMenu(event, force = false) {
+    // Verifica se o clique foi fora ou o fechamento é forçado pelo botão
+    if (!this.projetosList.contains(event.target) && !this.projetosButton.contains(event.target) || force) {
+      const items = this.projetosList.querySelectorAll('li');
+      this.animateMenuClose(items);
+      document.removeEventListener('click', this.closeMenu); // Remover o listener para evitar chamadas extras
+      this.isOpen = false;
+    }
+  }
 
-    outsideClick(this.projetosList, this.events, () => {
-      this.projetosList.classList.remove(this.activeClass);
-      this.projetosButtonButton.classList.remove(this.activeClass);
-     
+  animateMenuClose(items) {
+    gsap.to(items, {
+      opacity: 0,
+      duration: 0.3,
+      stagger: {
+        amount: 0.5,
+        from: "start"
+      },
+      onComplete: () => {
+        this.projetosList.classList.remove(this.activeClass);
+        this.projetosButton.classList.remove(this.activeClass);
+        gsap.set(items, { opacity: 0 });
+      }
     });
   }
 
   addMenuMobileEvents() {
-    this.events.forEach((evento) =>
-      this.projetosButton.addEventListener(evento, this.openMenu)
+    this.events.forEach(event =>
+      this.projetosButton.addEventListener(event, this.toggleMenu)
     );
   }
 
